157a1b4e4090e5d836e5133eb2f34f021a9d6371
jyb-maker <bjy7328@naver.com>
2025-01-09 11:08:59 +0900

    스프링부트 예제

---
+++ src/main/java/com/jbedu/test/SpringbootTestProject220250109JyApplication.java
@@ -0,0 +1,13 @@
+package com.jbedu.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTestProject220250109JyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTestProject220250109JyApplication.class, args);
+	}
+
+}
